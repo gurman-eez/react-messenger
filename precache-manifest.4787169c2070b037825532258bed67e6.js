8ad45504e189a192c7e69c16ed3b3ad29527a8f4
Elvin Zeinalabdziyeu <elvinzeinalabdziyeu@gmail.com>
2020-08-20 18:51:09 +0300

    Updates

---
+++ precache-manifest.4787169c2070b037825532258bed67e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9284df4cff5f6da9210284224b0191e1",
+    "url": "/react-messenger/index.html"
+  },
+  {
+    "revision": "f503f0f0ed28d01652f7",
+    "url": "/react-messenger/static/css/main.7b0dc002.chunk.css"
+  },
+  {
+    "revision": "5ea229fff45f411d0a8f",
+    "url": "/react-messenger/static/js/2.336a626d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-messenger/static/js/2.336a626d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f503f0f0ed28d01652f7",
+    "url": "/react-messenger/static/js/main.8381fcd7.chunk.js"
+  },
+  {
+    "revision": "884f5363db33036c0ead",
+    "url": "/react-messenger/static/js/runtime-main.44aea340.js"
+  }
+]);
